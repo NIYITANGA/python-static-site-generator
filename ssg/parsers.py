07e6257e104bf2ef0d4c9ab565f76e7b6b5d9788
Theophile Niyitanga <theo@Theophiles-Air.attlocal.net>
2021-09-22 10:46:31 -0400

    fixing error

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,13 +1,13 @@
 import shutil
 import sys
 
-from typing import List
+from typing import ContextManager, List
 from pathlib import Path
 
 from docutils.core import publish_parts
 from markdown import markdown
 
-from ssg.content import content
+from ssg.content import Content, content
 
 
 class Parser:
@@ -47,11 +47,11 @@ class MarkdownParser(Parser):
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML.Metadata: {}\n".format(path.name, content))
 
-class RestructuredTestParser(Parser):
+class RestructuredTextParser(Parser):
     extensions = [".rst"]
 
     def parse(self, path, source, dest):
-        content = Content.load(self.read(path))
+        content = ContextManager.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"]) 
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata:{}\n".format(path.name, content))
