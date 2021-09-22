28ee9b0e316e7fabe6b815e1cb8d366854e4ca9c
Theophile Niyitanga <theo@Theophiles-Air.attlocal.net>
2021-09-22 11:31:32 -0400

    fixing error

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from docutils.core import publish_parts
 from markdown import markdown
 
-from ssg.content import content
+from ssg.content import Content
 
 
 class Parser:
